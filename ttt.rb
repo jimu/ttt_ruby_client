15c8be56cf5b7fb04618337c4d625842e0486ec2
Jim Urbas <git@jimu.net>
2021-09-02 18:42:04 -0500

    Dumps all matches

--- ttt.rb
+++ ttt.rb
@@ -1,5 +1,25 @@
 #!/home/jimu/.rbenv/shims/ruby
 
+require 'net/http'
+require 'json'
+
 ver = "0.0.1 2021-09-02"
 
 puts "Tic-Tac-Toe Ruby Client v#{ver} Jim Urbas"
+
+url = "http://localhost:3016/matches"
+
+uri = URI(url)
+http = Net::HTTP.new(uri.host, uri.port)
+http.use_ssl = false
+
+request = Net::HTTP::Get.new(uri.path, {'Content-Type' => 'application/json'})
+
+response = http.request(request)
+
+matches = JSON.parse(response.body)
+
+
+matches.each do |m|
+  puts "#{m['id']} #{m['title']}"
+end
